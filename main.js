ab881530be72e6f2e1f4880bad651217e11acfbf
Chris Parnin <chris.parnin@gmail.com>
2018-02-22 09:42:30 -0500

    Improved workshop code.

--- main.js
+++ main.js
@@ -1,349 +1,38 @@
-var esprima = require("esprima");
-var options = {tokens:true, tolerant: true, loc: true, range: true };
-var faker = require("faker");
-var fs = require("fs");
-faker.locale = "en";
-var mock = require('mock-fs');
-var _ = require('underscore');
-var Random = require('random-js');
-
-function main()
-{
-	var args = process.argv.slice(2);
-
-	if( args.length == 0 )
-	{
-		args = ["subject.js"];
-	}
-	var filePath = args[0];
-
-	constraints(filePath);
-
-	generateTestCases()
-
-}
-
-var engine = Random.engines.mt19937().autoSeed();
-
-function createConcreteIntegerValue( greaterThan, constraintValue )
-{
-	if( greaterThan )
-		return Random.integer(constraintValue,constraintValue+10)(engine);
-	else
-		return Random.integer(constraintValue-10,constraintValue)(engine);
-}
-
-function Constraint(properties)
-{
-	this.ident = properties.ident;
-	this.expression = properties.expression;
-	this.operator = properties.operator;
-	this.value = properties.value;
-	this.altvalue = properties.altvalue;
-	this.funcName = properties.funcName;
-	// Supported kinds: "fileWithContent","fileExists"
-	// integer, string, phoneNumber
-	this.kind = properties.kind;
-}
-
-function fakeDemo()
-{
-	console.log( faker.phone.phoneNumber() );
-	console.log( faker.phone.phoneNumberFormat() );
-	console.log( faker.phone.phoneFormats() );
-}
-
-var functionConstraints =
-{
-}
-
-var mockFileLibrary = 
-{
-	pathExists:
-	{
-		'path/fileExists': {}
-	},
-	fileWithContent:
-	{
-		pathContent: 
-		{	
-  			file1: 'text content',
-		}
-	}
-};
-
-function initalizeParams(constraints)
-{
-	var params = {};
-	
-	// initialize params
-	for (var i =0; i < constraints.params.length; i++ )
-	{
-		var paramName = constraints.params[i];
-		params[paramName] = '\'\'';
-	}
-	return params;	
-}
-
-function fillParams(constraints,params,property)
-{
-	// plug-in values for parameters
-	for( var c = 0; c < constraints.length; c++ )
-	{
-		var constraint = constraints[c];
-		if( params.hasOwnProperty( constraint.ident ) )
-		{
-			params[constraint.ident] = constraint[property];
-		}
-	}
-}
-
-function generateTestCases()
-{
-
-	var content = "var subject = require('./subject.js')\nvar mock = require('mock-fs');\n";
-	for ( var funcName in functionConstraints )
-	{
-
-		var params = initalizeParams(functionConstraints[funcName])
-		var altparams = initalizeParams(functionConstraints[funcName])
-		
-		//console.log( params );
-
-		// update parameter values based on known constraints.
-		var constraints = functionConstraints[funcName].constraints;
-		// Handle global constraints...
-		var fileWithContent = _.some(constraints, {kind: 'fileWithContent' });
-		var pathExists      = _.some(constraints, {kind: 'fileExists' });
-
-		fillParams(constraints,params,"value")
-		fillParams(constraints,altparams,"altvalue")
-		
-		//console.log("ALT",altparams)
-		//console.log("P",params)
-
-		// Prepare function arguments.
-		var args = Object.keys(params).map( function(k) {return params[k]; }).join(",");
-		var altargs = Object.keys(altparams).map( function(k) {return altparams[k]; }).join(",");
-		
-		if( pathExists || fileWithContent )
-		{
-			content += generateMockFsTestCases(pathExists,fileWithContent,funcName, args);
-			// Bonus...generate constraint variations test cases....
-			content += generateMockFsTestCases(!pathExists,fileWithContent,funcName, args);
-			content += generateMockFsTestCases(pathExists,!fileWithContent,funcName, args);
-			content += generateMockFsTestCases(!pathExists,!fileWithContent,funcName, args);
-		}
-		else
-		{
-
-			console.log( altargs )
-			// Emit simple test case.
-			content += "subject.{0}({1});\n".format(funcName, args );
-			content += "subject.{0}({1});\n".format(funcName, altargs );
-		}
-
-	}
+/**
+ * NodeJS Test Generation Module
+ */
 
 
-	fs.writeFileSync('test.js', content, "utf8");
+// Core/NPM Modules
+const path    = require('path');
 
-}
 
-function generateMockFsTestCases (pathExists,fileWithContent,funcName,args) 
-{
-	var testCase = "";
-	// Build mock file system based on constraints.
-	var mergedFS = {};
-	if( pathExists )
-	{
-		for (var attrname in mockFileLibrary.pathExists) { mergedFS[attrname] = mockFileLibrary.pathExists[attrname]; }
-	}
-	if( fileWithContent )
-	{
-		for (var attrname in mockFileLibrary.fileWithContent) { mergedFS[attrname] = mockFileLibrary.fileWithContent[attrname]; }
-	}
+// Local Modules
+const constraints       = require('./src/constraint');
+const generateTestCases = require('./src/testgenerator');
 
-	testCase += 
-	"mock(" +
-		JSON.stringify(mergedFS)
-		+
-	");\n";
 
-	testCase += "\tsubject.{0}({1});\n".format(funcName, args );
-	testCase+="mock.restore();\n";
-	return testCase;
-}
+// Polyfills
+require('./src/format-polyfill');
 
-function constraints(filePath)
-{
-   var buf = fs.readFileSync(filePath, "utf8");
-	var result = esprima.parse(buf, options);
 
-	traverse(result, function (node) 
-	{
-		if (node.type === 'FunctionDeclaration') 
-		{
-			var funcName = functionName(node);
-			console.log("Line : {0} Function: {1}".format(node.loc.start.line, funcName ));
 
-			var params = node.params.map(function(p) {return p.name});
+/**
+ * Parse an input file and generate test cases for it.
+ */
+(module.exports.main = function() {
 
-			functionConstraints[funcName] = {constraints:[], params: params};
-
-			// Check for expressions using argument.
-			traverse(node, function(child)
-			{
-				if( child.type === 'BinaryExpression' && child.operator == "==")
-				{
-					if( child.left.type == 'Identifier' && params.indexOf( child.left.name ) > -1)
-					{
-						// get expression from original source code:
-						var expression = buf.substring(child.range[0], child.range[1]);
-						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
-
-						functionConstraints[funcName].constraints.push( 
-							new Constraint(
-							{
-								ident: child.left.name,
-								value: rightHand,
-								altvalue: !rightHand,
-								funcName: funcName,
-								kind: "integer",
-								operator : child.operator,
-								expression: expression
-							}));
-					}
-				}
-
-				if( child.type === 'BinaryExpression' && child.operator == "<" )
-				{
-					if( child.left.type == 'Identifier' && params.indexOf( child.left.name ) > -1)
-					{
-						// get expression from original source code:
-						var expression = buf.substring(child.range[0], child.range[1]);
-						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
-
-						functionConstraints[funcName].constraints.push( 
-							new Constraint(
-							{
-								ident: child.left.name,
-								value: parseInt(rightHand) - 1,
-								altvalue: parseInt(rightHand) +1,
-								funcName: funcName,
-								kind: "integer",
-								operator : child.operator,
-								expression: expression
-							}));
-					}
-				}
-
-				if( child.type == "CallExpression" && 
-					 child.callee.property &&
-					 child.callee.property.name =="readFileSync" )
-				{
-					for( var p =0; p < params.length; p++ )
-					{
-						if( child.arguments[0].name == params[p] )
-						{
-							functionConstraints[funcName].constraints.push( 
-							new Constraint(
-							{
-								ident: params[p],
-								value:  "'pathContent/file1'",
-								funcName: funcName,
-								kind: "fileWithContent",
-								operator : child.operator,
-								expression: expression
-							}));
-						}
-					}
-				}
-
-				if( child.type == "CallExpression" &&
-					 child.callee.property &&
-					 child.callee.property.name =="existsSync")
-				{
-					for( var p =0; p < params.length; p++ )
-					{
-						if( child.arguments[0].name == params[p] )
-						{
-							functionConstraints[funcName].constraints.push( 
-							new Constraint(
-							{
-								ident: params[p],
-								// A fake path to a file
-								value:  "'path/fileExists'",
-								funcName: funcName,
-								kind: "fileExists",
-								operator : child.operator,
-								expression: expression
-							}));
-						}
-					}
-				}
-
-			});
-
-			console.log( functionConstraints[funcName]);
-
-		}
-	});
-}
-
-function traverse(object, visitor) 
-{
-    var key, child;
-
-    visitor.call(null, object);
-    for (key in object) {
-        if (object.hasOwnProperty(key)) {
-            child = object[key];
-            if (typeof child === 'object' && child !== null) {
-                traverse(child, visitor);
-            }
-        }
+    // Parse file input, defaulting to subject.js if not provided
+    let args = process.argv.slice(2);
+    if( args.length === 0 ) {
+        args = ["subject.js"];
     }
-}
+    let filePath = path.resolve(args[0]);
 
-function traverseWithCancel(object, visitor)
-{
-    var key, child;
+    // Initialize constraints based on input file
+    let functionConstraints = constraints(filePath);
 
-    if( visitor.call(null, object) )
-    {
-	    for (key in object) {
-	        if (object.hasOwnProperty(key)) {
-	            child = object[key];
-	            if (typeof child === 'object' && child !== null) {
-	                traverseWithCancel(child, visitor);
-	            }
-	        }
-	    }
- 	 }
-}
+    // Generate test cases
+    generateTestCases(filePath, functionConstraints);
 
-function functionName( node )
-{
-	if( node.id )
-	{
-		return node.id.name;
-	}
-	return "";
-}
-
-
-if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
-}
-
-main();
-exports.main = main;
+})();
--- test.js
+++ test.js
@@ -1,19 +1,51 @@
-var subject = require('./subject.js')
-var mock = require('mock-fs');
-subject.inc('',undefined);
-subject.weird('','','',"strict");
-mock({"path/fileExists":{},"pathContent":{"file1":"text content"}});
-	subject.fileTest('path/fileExists','pathContent/file1');
-mock.restore();
-mock({"pathContent":{"file1":"text content"}});
-	subject.fileTest('path/fileExists','pathContent/file1');
-mock.restore();
-mock({"path/fileExists":{}});
-	subject.fileTest('path/fileExists','pathContent/file1');
-mock.restore();
-mock({});
-	subject.fileTest('path/fileExists','pathContent/file1');
-mock.restore();
-subject.normalize('');
-subject.format('','','');
-subject.blackListNumber('');
+let subject = require('/Users/gameweld/classes/519/workshops/TestGeneration/subject.js')
+let mock = require('mock-fs');
+try { subject.inc('', undefined); } catch (e) {} 
+try { subject.inc('', NaN); } catch (e) {} 
+try { subject.weird('', '', '', "strict"); } catch (e) {} 
+try { subject.weird('', '', '', 'NEQ - strict'); } catch (e) {} 
+try { subject.weird('', '', '', "strict"); } catch (e) {} 
+try { subject.weird('', '', '', 'NEQ - strict'); } catch (e) {} 
+try{
+	mock({emptyDir:mock.directory(),nonEmptyDir:mock.directory({ items: { file: mock.file() } }),file:mock.file(),pathContent:{file1:new Buffer('abc'),someDir:mock.directory()}});
+		subject.fileTest('', 'pathContent/file1');
+	mock.restore();
+} catch(e) {}
+try{
+	mock({pathContent:{file1:new Buffer('abc'),someDir:mock.directory()}});
+		subject.fileTest('', 'pathContent/file1');
+	mock.restore();
+} catch(e) {}
+try{
+	mock({emptyDir:mock.directory(),nonEmptyDir:mock.directory({ items: { file: mock.file() } }),file:mock.file()});
+		subject.fileTest('', 'pathContent/file1');
+	mock.restore();
+} catch(e) {}
+try{
+	mock({});
+		subject.fileTest('', 'pathContent/file1');
+	mock.restore();
+} catch(e) {}
+try{
+	mock({emptyDir:mock.directory(),nonEmptyDir:mock.directory({ items: { file: mock.file() } }),file:mock.file(),pathContent:{file1:new Buffer('abc'),someDir:mock.directory()}});
+		subject.fileTest('', 'pathContent/someDir');
+	mock.restore();
+} catch(e) {}
+try{
+	mock({pathContent:{file1:new Buffer('abc'),someDir:mock.directory()}});
+		subject.fileTest('', 'pathContent/someDir');
+	mock.restore();
+} catch(e) {}
+try{
+	mock({emptyDir:mock.directory(),nonEmptyDir:mock.directory({ items: { file: mock.file() } }),file:mock.file()});
+		subject.fileTest('', 'pathContent/someDir');
+	mock.restore();
+} catch(e) {}
+try{
+	mock({});
+		subject.fileTest('', 'pathContent/someDir');
+	mock.restore();
+} catch(e) {}
+try { subject.normalize(''); } catch (e) {} 
+try { subject.format('', '', ''); } catch (e) {} 
+try { subject.blackListNumber(''); } catch (e) {} 
